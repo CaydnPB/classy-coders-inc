fdbfa0cc87b9198fceb9e4bf3969efb3f2c5515c
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-10-25 15:26:30 +0100

    Add SalesPerson Class

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,8 +1,23 @@
-class SalesPerson {
+const { Employees } = require("./Employees")
 
+class SalesPerson extends Employees {
+    #salary;
+    #isHired;
+    #clients;
+    #totalSales
+    constructor(name, position, salary, clients) {
+        super(name, position, salary);
+        this.#clients = clients;
+        this.#totalSales = 0;
+    }
 
+    getSalesNumber() {
+        return this.#totalSales;
+    }
 
-    
+    makeSale(amount) {
+        this.#totalSales += amount;
+    }
 }
 
 module.exports = {
