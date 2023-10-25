8cdf7a4d1a7018a621f3c27530f90e92cdccbd31
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-10-25 15:32:43 +0100

    Update Employees class

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,11 +1,23 @@
 class Employees {
     #salary;
     #isHired;
+    static #allEmployees = [];
+    static getEmployees(){
+        return this.#allEmployees;
+    }
+    static getTotalPayroll(){
+        let total = 0;
+        for (const employee of this.getEmployees()) {
+            total += employee.#salary;
+        }
+        return total;
+    }
     constructor(name, position, salary) {
         this.name = name;
         this.position = position;
         this.#salary = salary;
         this.#isHired = true;
+        Employees.#allEmployees.push(this);
     }
 
     getSalary() {
